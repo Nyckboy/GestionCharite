041256053a1fee018e3dd5feb6f330753ed77acb
nyckboy <abasside132@gmail.com>
2026-03-03 22:24:17 +0000

    first commit

---
+++ src/test/java/com/project/GestionCharite/GestionChariteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GestionCharite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionChariteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
