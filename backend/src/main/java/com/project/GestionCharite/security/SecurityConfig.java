d9c4d00dfeae85b1b35e9cd9b269f3ff29d359f0
nyckboy <abasside132@gmail.com>
2026-04-13 18:55:17 +0100

    [backend]: fixing cors so the front can talk to the back

--- backend/src/main/java/com/project/GestionCharite/security/SecurityConfig.java
+++ backend/src/main/java/com/project/GestionCharite/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable)
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
